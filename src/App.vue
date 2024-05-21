7d8d6076bef44fc3087494196b7da07b57a0a23e
kevinalfariza <kevinalfariza10.com>
2024-05-21 21:25:26 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,174 @@
+<template>
+  <q-layout view="hHh lpR fFf">
+    <q-header elevated class="bg-pink text-white" height-hint="98">
+      <div class="q-pa-md q-gutter-sm">
+        <q-bar class="bg-black text-white">
+          <q-btn dense flat icon="fab fa-apple" />
+          <div class="text-weight-bold">App</div>
+          <div class="cursor-pointer gt-md">File</div>
+          <div class="cursor-pointer gt-md">Edit</div>
+          <div class="cursor-pointer gt-md">View</div>
+          <div class="cursor-pointer gt-md">Window</div>
+          <div class="cursor-pointer gt-md">Help</div>
+          <q-space />
+          <q-btn dense flat icon="airplay" class="gt-xs" />
+          <q-btn dense flat icon="battery_charging_full" />
+          <q-btn dense flat icon="wifi" />
+          <div>9:41</div>
+          <q-btn dense flat icon="search" />
+          <q-btn dense flat icon="list" />
+        </q-bar>
+
+        <q-bar>
+          <q-btn dense flat round icon="lens" size="8.5px" color="red" />
+          <q-btn dense flat round icon="lens" size="8.5px" color="yellow" />
+          <q-btn dense flat round icon="lens" size="8.5px" color="green" />
+          <div class="col text-center text-weight-bold">My-App</div>
+        </q-bar>
+      </div>
+    </q-header>
+
+    <q-drawer show-if-above v-model="leftDrawerOpen" side="left" bordered>
+      <!-- drawer content -->
+    </q-drawer>
+
+    <q-drawer show-if-above v-model="rightDrawerOpen" side="right" bordered>
+      <!-- drawer content -->
+    </q-drawer>
+
+    <q-page-container>
+      <q-page class="q-pa-md">
+        <q-carousel
+          animated
+          v-model="slide"
+          arrows
+          navigation
+          infinite
+        >
+          <q-carousel-slide :name="1" img-src="https://cdn.quasar.dev/img/mountains.jpg" />
+          <q-carousel-slide :name="2" img-src="https://cdn.quasar.dev/img/parallax1.jpg" />
+          <q-carousel-slide :name="3" img-src="https://cdn.quasar.dev/img/parallax2.jpg" />
+          <q-carousel-slide :name="4" img-src="https://cdn.quasar.dev/img/quasar.jpg" />
+        </q-carousel>
+        
+        <div class="q-pa-md row items-start q-gutter-md">
+          <q-card class="my-card" flat bordered>
+            <q-img src="https://cdn.quasar.dev/img/parallax2.jpg" />
+
+            <q-card-section>
+              <div class="text-overline text-orange-9">Overline</div>
+              <div class="text-h5 q-mt-sm q-mb-xs">Title</div>
+              <div class="text-caption text-grey">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </div>
+            </q-card-section>
+
+            <q-card-actions>
+              <q-btn flat color="primary" label="Share" />
+              <q-btn flat color="secondary" label="Book" />
+
+              <q-space />
+
+              <q-btn
+                color="grey"
+                round
+                flat
+                dense
+                :icon="expanded ? 'keyboard_arrow_up' : 'keyboard_arrow_down'"
+                @click="expanded = !expanded"
+              />
+            </q-card-actions>
+
+            <q-slide-transition>
+              <div v-show="expanded">
+                <q-separator />
+                <q-card-section class="text-subtitle2">
+                  {{ lorem }}
+                </q-card-section>
+              </div>
+            </q-slide-transition>
+          </q-card>
+        </div>
+      </q-page>
+    </q-page-container>
+
+    <q-footer elevated class="bg-grey-8 text-white">
+      <q-toolbar>
+        <q-toolbar-title>
+          <q-avatar>
+            <img src="https://i.pinimg.com/originals/b3/ea/ee/b3eaee4c8d9ca38eb055b32f31bbc8ba.png">
+          </q-avatar>
+          <div>uir unggul</div>
+        </q-toolbar-title>
+      </q-toolbar>
+      <div class="q-pa-md q-gutter-sm">
+        <q-tree
+          :nodes="simple"
+          node-key="label"
+        />
+      </div>
+    </q-footer>
+  </q-layout>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const leftDrawerOpen = ref(false)
+const rightDrawerOpen = ref(false)
+const slide = ref(1)
+const expanded = ref(false)
+const lorem = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+
+const simple = ref([
+  {
+    label: 'Satisfied customers (with avatar)',
+    avatar: 'https://cdn.quasar.dev/img/boy-avatar.png',
+    children: [
+      {
+        label: 'Good food (with icon)',
+        icon: 'restaurant_menu',
+        children: [
+          { label: 'Quality ingredients' },
+          { label: 'Good recipe' }
+        ]
+      },
+      {
+        label: 'Good service (disabled node with icon)',
+        icon: 'room_service',
+        disabled: true,
+        children: [
+          { label: 'Prompt attention' },
+          { label: 'Professional waiter' }
+        ]
+      },
+      {
+        label: 'Pleasant surroundings (with icon)',
+        icon: 'photo',
+        children: [
+          {
+            label: 'Happy atmosphere (with image)',
+            img: 'https://cdn.quasar.dev/img/logo_calendar_128px.png'
+          },
+          { label: 'Good table presentation' },
+          { label: 'Pleasing decor' }
+        ]
+      }
+    ]
+  }
+])
+
+const toggleLeftDrawer = () => {
+  leftDrawerOpen.value = !leftDrawerOpen.value
+}
+
+const toggleRightDrawer = () => {
+  rightDrawerOpen.value = !rightDrawerOpen.value
+}
+</script>
+
+<style lang="sass" scoped>
+.my-card
+  width: 100%
+  max-width: 350px
+</style>
